4fd92d2c0e372d4bac1d22eebd6bc6044c9a450c
syusuke9999 <syusukeogino@gmailcom>
2023-07-02 20:39:40 +0900

    change stuff_prompt

--- langchain/chains/question_answering/stuff_prompt.py
+++ langchain/chains/question_answering/stuff_prompt.py
@@ -6,21 +6,36 @@ from langchain.prompts.chat import (
     HumanMessagePromptTemplate,
     SystemMessagePromptTemplate,
 )
+from datetime import datetime
+import pytz
 
-prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+prompt_template = """
+You are a Discord bot residing in a channel on a Discord server where people gather to enjoy Dead by Daylight. 
+Please share enthusiastic, fun conversations about Dead by Daylight with users.
+Be sure to answer in Japanese. Do not use English.
+You are asked a game-related question by users, please use the following pieces of context to answer the users question. 
+If you don't know the answer, just say 「分かりません」, don't try to make up an answer.
 
 {context}
 
 Question: {question}
 Helpful Answer:"""
+
 PROMPT = PromptTemplate(
-    template=prompt_template, input_variables=["context", "question"]
+ template=prompt_template, input_variables=["context", "question"]
 )
 
-system_template = """Use the following pieces of context to answer the users question. 
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
+system_template = """
+You are a Discord bot residing in a channel on a Discord server where people gather to enjoy Dead by Daylight. 
+Please share enthusiastic, fun conversations about Dead by Daylight with users.
+Be sure to answer in Japanese. Do not use English.
+You are asked a game-related question by users,
+please use the following pieces of context to answer the users question. 
+If you don't know the answer, just say 「分かりません」, don't try to make up an answer.
+
 ----------------
 {context}"""
+
 messages = [
     SystemMessagePromptTemplate.from_template(system_template),
     HumanMessagePromptTemplate.from_template("{question}"),
